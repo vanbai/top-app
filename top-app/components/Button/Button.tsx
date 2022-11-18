f98db6738feff943a988d17082df7b5ef8486e6b
vanbai <van7bai@gmail.com>
2022-11-18 18:16:52 +0400

    add buttons

---
+++ top-app/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import styles from "./Button.module.css";
+import { ButtonProps } from "./Button.props";
+import cn from 'classnames';
+
+export const Button = ({ children, appearance, className, ...props }: ButtonProps): JSX.Element => {
+    return (
+        <button className={cn(styles.button, className, {
+            [styles.primary]: appearance == 'primary',
+            [styles.ghost]: appearance == 'ghost'
+        })}
+        {...props}
+        >
+            {children}
+        </button>
+    )
+};
